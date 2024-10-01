a37b5b0e8189c4036f5bb3f62999a00244228399
Claire Robson <claire.robson1@barclays.com>
2024-10-01 14:54:24 +0100

    Music update

--- classes/Music.js
+++ classes/Music.js
@@ -1,6 +1,19 @@
 // import the Media class:
-
+const Media = require("./Media.js")
 // create your Music class:
-
+class Music extends Media{
+    constructor(title, genre, year, artist, length){
+        super(title, genre, year);
+        this.artist = artist;
+        this.length = length;
+    }
+    summary (){
+        return `Title: ${title}, Artist: ${artist}, Year: ${year}, Genre: ${genre}`
+    }
+    static shortestAlbum () {
+        const shortestValue = Math.min(this.length);
+        return shortestValue;
+    }
+}
 // don't change below
 module.exports = Music;
